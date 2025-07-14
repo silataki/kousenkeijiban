2ff47a0a61a8aef07bc7aeb65a48021889a5edb1
tooooohu <k21149hf@apps.kct.ac.jp>
2025-07-14 13:48:15 +0900

    Update logout_input.php

--- src/main/login/logout_input.php
+++ src/main/login/logout_input.php
@@ -3,9 +3,14 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-
 ?>
+
 <p>ログアウトしますか？</p>
+
+<?php
+echo '<img src="/images/logout_cry.png" alt="ログアウト確認画像" style="max-width: 300px; height: auto;">';
+?>
+
 <a href="logout_output.php">ログアウト</a>
 
 <?php
